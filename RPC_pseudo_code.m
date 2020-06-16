276ae064f7447d09f69db9a0d510d80e8b70eadf
Lau77 <l.beijnen@hotmail.com>
2020-06-16 23:59:18 +0200

    updated RPC

--- RPC_pseudo_code.m
+++ RPC_pseudo_code.m
@@ -3,41 +3,65 @@
 % initialize Q available die bij P available hoort
 % count = 0
 % Opti_switch = FALSE
-clear all
+clear
 close all
-%% Initialize profiles
-dt = 200e-3;                              % 10 ms sample time
-Tfinal = 300;                             % simulation will last 200 s
+%% Initializations profiles %%
+
+dt = 200e-3;                              % 200 ms sample time
+Tfinal = 300;                             % simulation will last 300 s
 Ntb = 13;                                 % number of wind turbine strings
 Npv = 4;                                  % number of PV module strings
 t = 0:dt:Tfinal;                          % time vector
 Q_sp = zeros(length(t), 1);               % initialize input vector with Q set points
+k_p = 3;                                  % proportional gain VSPI controller
+T_i = 0.7;                                % integral time constant VSPI controller
+beta = 2;                                 % parameter beta for VSPI
+sigma = 2;                                % parameter sigma for VSPI
+error = zeros(length(t),1);               % difference between Q set point TSO and Q delivered by PPM
+u = zeros(length(t),1);                   % output VSPI controller
+Q_sp_pcc = zeros(1,length(t));            % TSO Q set point request
+Q_ppm = zeros(1,length(t));               % Q delivered by PPM at PCC
+Q_big = zeros(1,length(t));               % Q that will be devided over the strings
+threshold_opt = 3;                        % threshold for implementing optimization set point
+Opti_switch = FALSE;                      % logic variable that determines optimization usage
 
-% Set point times and values %
-sp_1 = 0e6;                               % first setpoint 0 MVar
-sp_2 = 50e6;                              % second setpoint 50 MVar
-t_sp2 = 100;                              % second setpoint at 100 seconds
-sp_3 = 0e6;                               % third setpoint 0 MVar
-t_sp3 = 200;                              % third setpoint at 200 seconds
-sp_4 = 0e6;                               % fourth setpoint 0 MVar
-t_sp4 = 250;                              % fourth setpoint at 250 seconds
+%% Set point times and values %%
 
-windspeed = linspace(0,30,300);           % wind profile      
-irradiance = linspace(0,680,300);         % solar profile
-Q_sp_PCC = zeros(1,length(t));      % TSO Q set point request
+sp_1 = 0;                               % first setpoint 0 MVar
+t_sp1 = 0;                              % first set point at 0 seconds
+sp_2 = 50;                              % second setpoint 50 MVar
+t_sp2 = 100;                            % second setpoint at 100 seconds
+sp_3 = 0;                               % third setpoint 0 MVar
+t_sp3 = 200;                            % third setpoint at 200 seconds
+sp_4 = 0;                               % fourth setpoint 0 MVar
+t_sp4 = 250;                            % fourth setpoint at 250 seconds
+
+setpoint_values = [sp_1 sp_2 sp_3 sp_4];    % vector containing set points
+setpoint_times = [t_sp1 t_sp2 t_sp3 t_sp4]; % vector containing set point times
+
+%% setting up input vector P_sp_PCC containing the setpoints %%
+
+idx_begin = 1;
+for i = 2:length(setpoint_values)
+    [val,idx_end]=min(abs(t-setpoint_times(i)));
+    Q_sp_pcc(idx_begin:idx_end-1)= setpoint_values(i-1);
+    idx_begin = idx_end;
+end
+Q_sp_pcc(idx_begin:end) = setpoint_values(end);
 
 %% Initialize wind capability
+
 stepsize = 0.1;
 vmin = 0;
 vmax = 30;
 v = vmin:stepsize:vmax;
 [Pwtg_string,Qwtg_string] = compute_pq_wtg(v);
 endsamp_string = 25/stepsize + 1;
-% figure(1)
-% plot(P(:,1),Q(:,1))
+
 
 %% Initialize PV capability
 
+irradiance = linspace(0,680,300);         % solar profile for capability curver
 [Ppvg_string,Qpvg_string] = compute_pq_pvg(irradiance,Npv);
 Structure_Size = Ntb+Npv;  %number of elements
 PQ(Structure_Size) = struct();
@@ -51,23 +75,24 @@ for j = 1:Ntb+Npv
     end   
 end
 
-% plot capability curves %
+%% plot capability curves %%
 
-for i = 1:Ntb+Npv
-figure(1)
-subplot(5,4,i)
-if i<=Ntb
-    plot(PQ(i).P(1:endsamp_string),PQ(i).Q(1:endsamp_string))
-else
-    plot(PQ(i).P,PQ(i).Q)
-end
-xlabel('P [MW]')
-ylabel('Q [MVAr]')
-end
+% for i = 1:Ntb+Npv
+% figure(1)
+% subplot(5,4,i)
+%     if i<=Ntb
+%         plot(PQ(i).P(1:endsamp_string),PQ(i).Q(1:endsamp_string))
+%     else
+%         plot(PQ(i).P,PQ(i).Q)
+%     end
+% xlabel('P [MW]')
+% ylabel('Q [MVAr]')
+% end
 
 %% Obtain Q available for each strings %%
 
 % load in P of each string at every time instant
+%APC = P_current_string.';
 APC = 10*ones(Ntb+Npv, length(t));
 
 % convert outcome from previous steps into Qavailable for each string at every
@@ -80,23 +105,29 @@ for i = 1:Ntb+Npv
     end
 end
 
-% for t
-% 
+%% Iterate over time %%
+
+for j = 1:length(t)
+ 
 % (depends on mode)if the Reactive Power Exchange at the PCC or the Voltage Deviation at the PCC exceeds the predefined 
 % Disturbance Threshold, the Reactive Power Support of the PPM must be maintained for at least 15 minutes.
-%
-% error =  Q_sp_PCC - Qppm
-% u(t) = VSPI(error)
-% u(t) + TSO_setpoint
-% 
-% Ben ik stabiel?
-% if error < waarde
-%     ++count
-%     if count = 100
-%         implement opti setpoints
-%         Opti_switch = TRUE
-% else
-%     count = 0
+
+error(j) =  Q_sp_pcc(j) - Qppm(j);
+    
+u(j) = k_p*( error(j) + trapz( (error/T_i)*exp(-(error^2)/(2*beta^(2)*sigma^2)) ) ); 
+
+Q_big(j) = u(j) + Q_sp_pcc(j);
+
+    % Check if set point is reached and stable %
+    count = 0;
+    if error(j) < threshold
+        count = count+1;
+            if count == 100
+                Opti_switch = TRUE; % implement optimazation setpoints
+            end         
+    else
+        count = 0;
+    end
 % 
 % If Opti_switch = FALSE
 % Dat verdelen over strings adhv DF (Let op Q_a_i):
@@ -114,4 +145,4 @@ end
 %         Interrupt ON
 %     DF = opti setpoint
 % 
-% ++t
+end
