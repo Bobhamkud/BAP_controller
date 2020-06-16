f86011e5ef50ad169b92068d445cfe21f5d35f3b
Lau77 <l.beijnen@hotmail.com>
2020-06-16 23:23:42 +0200

    updated base system_17 and completed active power

--- active_power_v5.m
+++ active_power_v5.m
@@ -2,8 +2,8 @@ clear
 close all
 %% Initializations %%
 
-dt = 100e-3;                                    % 10 ms sample time
-Tfinal = 200;                                   % simulation will last 200 s
+dt = 1000e-3;                                   % 10 ms sample time
+Tfinal = 18000;                                 % simulation will last 200 s
 Ntb = 13;                                       % number of wind turbine strings
 Npv = 4;                                        % number of PV module strings
 t = 0:dt:Tfinal;                                % time vector
@@ -13,18 +13,22 @@ P_sp_string = zeros(length(t),Ntb+Npv);         % initialize active power set po
 P_current_string  = zeros(length(t), Ntb+Npv);  % initialize current active power produced per string over time
 a_p = 70;                                       % initialize a_p constant for the distribution factors 
 P_pcc = zeros(1,length(t));                     % initialize vector with current PCC value
-case_log = zeros(1,length(t));                  % initialize case log 
+case_log = zeros(1,length(t));                  % initialize case log
+P_a_tot = zeros(1,length(t));                   % total available power
+P_a_wind = zeros(1,length(t));                  % total available wind power
+Run_pf_setting = mpoption('verbose',0,'out.all',0); % hide MATPOWER output
+load('agreed_profiles.mat')                         % load wind and solar profiles
 
 %% Set point times and values %%
 
-sp_1 = 200;                              % first set point 200 MW
+sp_1 = 70;                               % first set point 70 MW
 t_sp1 = 0;                               % first set point at 0 seconds
-sp_2 = 75;                               % second set point 400 MW
-t_sp2 = 40;                              % second set point at 40 seconds
-sp_3 = 0;                                % third set point 200 MW
-t_sp3 = 110;                             % third set point at 110 seconds
-sp_4 = 100;                              % fourth set point 100 MW
-t_sp4 = 160;                             % fourth set point at 160 seconds
+sp_2 = 70;                               % second set point 70 MW
+t_sp2 = 4500;                            % second set point at 4500 seconds
+sp_3 = 110;                              % third set point 110 MW
+t_sp3 = 9000;                            % third set point at 9000 seconds
+sp_4 = 55;                               % fourth set point 55 MW
+t_sp4 = 13500;                           % fourth set point at 13500 seconds
 
 setpoint_values = [sp_1 sp_2 sp_3 sp_4];    % vector containing set points
 setpoint_times = [t_sp1 t_sp2 t_sp3 t_sp4]; % vector containing set point times
@@ -42,16 +46,18 @@ P_sp_pcc(idx_begin:end) = setpoint_values(end);
 
 v_profile = zeros(1,length(t));
 for i =1:length(v_profile)
-v_profile(i) = normrnd(20,0.1);
+v_profile(i) = normrnd(7.7,0.1);
 end
+v_profile(1:end) = windspeed(1:length(t));
 [P_a_string(:,1:Ntb),Qwtg_string_profile] = compute_pq_wtg(v_profile);
 
 %% Initialize irradiance profile  %%
 
 solar_profile = zeros(1,length(t));
 for i =1:length(solar_profile)
-    solar_profile(i) = normrnd(680,50);
+    solar_profile(i) = normrnd(680,10);
 end
+solar_profile(1:end) = irradiance(1:length(t));
 [P_a_pv,Qpvg_string_profile] = compute_pq_pvg(solar_profile,Npv);
 P_a_string(:,Ntb+1:end) = P_a_pv.';
 
@@ -61,7 +67,7 @@ P_a_string(:,Ntb+1:end) = P_a_pv.';
 system = loadcase('system_17');
 system.gen(6:end,[2,9,10]) = [(P_a_string(1,1:Ntb)-5).' (P_a_string(1,1:Ntb)-5).' (P_a_string(1,1:Ntb)-5).'];
 system.gen(2:5,[2,9,10]) = [(P_a_string(1,Ntb+1:end)-5).' (P_a_string(1,Ntb+1:end)-5).' (P_a_string(1,Ntb+1:end)-5).'];
-current_values = runpf(system);
+current_values = runpf(system, Run_pf_setting);
 P_current_string(1,1:Ntb) = current_values.gen(6:18,2);
 P_current_string(1,Ntb+1:end) = current_values.gen(2:5,2);
 P_pcc(1) = -1 * current_values.branch(1,14);
@@ -70,7 +76,7 @@ P_pcc(1) = -1 * current_values.branch(1,14);
 case_1=0;
 case_2=0;
 case_3=0;
-    
+case_3b=0;   
 
 % beta opslaan!!!
 
@@ -78,30 +84,42 @@ case_3=0;
 
 for j = 1:length(t)-1
      
-    P_a_tot = sum(P_a_string(j,:));
+    P_a_tot(j) = sum(P_a_string(j,:));
+    P_a_wind(j) = sum(P_a_string(j,1:13));
     
     Delta_pcc = P_sp_pcc(j)-P_pcc(j);
 
     % check for which case applies %
     if j>1
-        if P_sp_pcc(j) < P_a_tot && P_sp_pcc(j)<P_sp_pcc(j-1)
-            case_1 = 0;
-            case_2 = 0;
-            case_3 = 1;
-            case_log(j) = 3;
-        elseif P_sp_pcc(j) < P_a_tot
+        if P_sp_pcc(j) < P_a_tot(j) && P_sp_pcc(j)<P_sp_pcc(j-1)
+            if P_sp_pcc(j) < P_a_tot(j) && P_sp_pcc(j)<P_sp_pcc(j-1) && case_2 == 1
+                case_1 = 0;
+                case_2 = 0;
+                case_3 = 1;
+                case_log(j) = 3;
+                case_3b = 1;
+            else
+                case_1 = 0;
+                case_2 = 0;
+                case_3 = 1;
+                case_log(j) = 3;
+                case_3b = 0;
+            end
+        elseif P_sp_pcc(j) < P_a_tot(j)
             case_1 = 1;
             case_2 = 0;
             case_3 = 0;
+            case_3b  =0;
             case_log(j) = 1;
         else
             case_1 = 0;
-            case_2 = 2;
+            case_2 = 1;
             case_3 = 0;
+            case_3b = 0;
             case_log(j) = 2;
         end
     else
-        if P_sp_pcc(j) < P_a_tot
+        if P_sp_pcc(j) < P_a_tot(j)
                 case_1 = 1;
         else
                 case_2 = 1;
@@ -119,8 +137,7 @@ for j = 1:length(t)-1
     elseif case_2 == 1
         
         beta = (P_a_string(j,:) - P_current_string(j,:))/Delta_pcc;
-        %beta = ones(1,Ntb+Npv)* (1/(Ntb+Npv));
-        
+        %beta = ones(1,Ntb+Npv)* (1/(Ntb+Npv));       
         P_sp_string(j,:) = P_current_string(j,:) + beta*Delta_pcc;
         
     else
@@ -135,17 +152,24 @@ for j = 1:length(t)-1
             end
         end
         
-        Psub = ( P_sp_pcc(j-1)-P_sp_pcc(j) ) / Ns_max;  % amount of active power that will be deducted
-                                                        % from turbines producing at their max
+        if case_3b == 1
+            Psub = ( sum(P_current_string(j,:))- P_sp_pcc(j) ) / Ns_max;
+        else
+            Psub = ( P_sp_pcc(j-1)-P_sp_pcc(j) ) / Ns_max;  % amount of active power that will be deducted
+                                                            % from turbines producing at their max
+        end
         
         % reduce the output of the wind turbines producing at maximum % 
         for i = 1:Ntb+Npv
             if Ps_op_max(i)== 1
                 P_sp_string(j,i) = P_current_string(j,i) - Psub;
+            else
+                P_sp_string(j,i) = P_current_string(j,i);
             end
         end
         if Ns_max == 0
             beta = calc_DF(P_current_string(j,:),P_a_string(j,:),a_p);
+            %beta = ones(1,Ntb+Npv)* (1/(Ntb+Npv));
             P_sp_string(j,:) = P_current_string(j,:) + beta*Delta_pcc;
         end
         
@@ -180,7 +204,7 @@ for j = 1:length(t)-1
     % apply set points %
     system.gen(6:end,[2,9,10]) = [P_sp_string(j,1:Ntb).' P_sp_string(j,1:Ntb).' P_sp_string(j,1:Ntb).'];
     system.gen(2:5,[2,9,10]) = [P_sp_string(j,Ntb+1:end).' P_sp_string(j,Ntb+1:end).' P_sp_string(j,Ntb+1:end).'];
-    current_values = runpf(system);
+    current_values = runpf(system, Run_pf_setting);
     P_current_string(j+1,1:Ntb) = current_values.gen(6:18,2);     
     P_current_string(j+1,Ntb+1:end) = current_values.gen(2:5,2);
     P_pcc(j+1) = -1 * current_values.branch(1,14);
@@ -190,18 +214,20 @@ end
 % Plot set point response % 
 plot(t,P_pcc)
 hold on
-plot(t,P_sp_pcc)
+plot(t,P_sp_pcc,t,P_a_tot, t, P_a_wind)
 title('Active power response for high wind profile', 'FontSize', 24)
 xlabel('Time [s]', 'FontSize', 24)
 ylabel('Power [MW]', 'FontSize', 24)
-legend('PPM Response','TSO Set point')
-lgd.FontSize = 24;
+legend('PPM Response','TSO Set point', 'Total Available power', 'Wind Power')
+legend.FontSize = 24;
+
 % Plot wind profies % 
 figure(2)
 plot(t,v_profile)
 title('Wind profile', 'FontSize', 24)
 xlabel('Time [s]', 'FontSize', 24)
 ylabel('Wind speed [m/s]', 'FontSize', 24)
+
 % Plot irradiance profile %
 figure(3)
 plot(t,solar_profile)
@@ -209,11 +235,13 @@ title('Irradiance profile', 'FontSize', 24)
 xlabel('Time [s]', 'FontSize', 24)
 ylabel('Irradiance [W/m^{2}]', 'FontSize', 24)
 figure(4)
+
 % Plot case occurences %
 plot(t,case_log*100)
 title('Cases that occur during simulation', 'FontSize', 24)
 xlabel('Time [s]', 'FontSize', 24)
 ylabel('Case nuber times 100', 'FontSize', 24)
+
 % Plot individual set points % 
 figure(5)
 plot(t,P_sp_string(:,1), t,P_sp_string(:,2),t,P_sp_string(:,3),t,P_sp_string(:,4))
